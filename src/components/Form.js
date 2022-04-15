c16a18d37ff2fd74ed3ea8a2d2ccdb6f3ba384e4
DaSilvaMarco <tdasilva.marco@gmail.com>
2022-04-15 15:23:01 +0200

    likes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,27 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
+import Loading from "./Loading";
 
 const Form = () => {
   const [moviesData, setMoviesData] = useState([]);
-  const [search, setSearch] = useState("code");
+  const [search, setSearch] = useState(null);
   const [sortGoodBad, setSortGoodBad] = useState(null);
 
   useEffect(() => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=e0026bdb43933e1d07ddc751057c1a99&query=${search}&language=fr-FR`
-      )
-      .then((res) => setMoviesData(res.data.results));
+    if (search) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=e0026bdb43933e1d07ddc751057c1a99&query=${search}&language=fr-FR`
+        )
+        .then((res) => setMoviesData(res.data.results));
+    } else {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/popular?api_key=e0026bdb43933e1d07ddc751057c1a99&language=fr-FR&page=1`
+        )
+        .then((res) => setMoviesData(res.data.results));
+    }
   }, [search]);
 
   return (
@@ -45,19 +54,21 @@ const Form = () => {
         </div>
       </div>
       <div className="result">
-        {moviesData
-          .slice(0, 12)
-          .sort((a, b) => {
-            if (sortGoodBad === "goodToBad") {
-              return b.vote_average - a.vote_average;
-            } else if (sortGoodBad === "badToGood") {
-              return a.vote_average - b.vote_average;
-            }
-            return null;
-          })
-          .map((movie) => (
-            <Card key={movie.id} movie={movie} />
-          ))}
+        {moviesData.length > 0 ? (
+          moviesData
+            .slice(0, 12)
+            .sort((a, b) => {
+              if (sortGoodBad === "goodToBad") {
+                return b.vote_average - a.vote_average;
+              } else if (sortGoodBad === "badToGood") {
+                return a.vote_average - b.vote_average;
+              }
+              return null;
+            })
+            .map((movie) => <Card key={movie.id} movie={movie} />)
+        ) : (
+          <Loading />
+        )}
       </div>
     </div>
   );
--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import Header from '../components/Header';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card";
+import Header from "../components/Header";
+import Loading from "../components/Loading";
 
 const UserList = () => {
-    return (
-        <div>
-            <Header />
-            <h1>UserList</h1>
-        </div>
-    );
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    let moviesId = window.localStorage.movies
+      ? window.localStorage.movies.split(",")
+      : [];
+
+    for (let i = 0; i < moviesId.length; i++) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=e0026bdb43933e1d07ddc751057c1a99&language=fr-FR&external_source=imdb_id`
+        )
+        .then((res) => setListData((listData) => [...listData, res.data]));
+    }
+  }, []);
+
+  return (
+    <div className="user-list-page">
+      <Header />
+      <div className="result">
+        {listData.length > 0 ? (
+          listData.map((movie) => <Card key={movie.id} movie={movie} />)
+        ) : (
+          <Loading />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default UserList;
